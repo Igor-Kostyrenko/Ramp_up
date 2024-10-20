bdcbe8caf50569aee1dd9a0e9d8d5dad3afdcadd
Igor Kostyrenko <115296463+Igor-Kostyrenko@users.noreply.github.com>
2024-10-20 12:45:08 +0300

    Update index with ASM.php

--- rump_up_files/MySQL/index with ASM.php
+++ rump_up_files/MySQL/index with ASM.php
@@ -26,34 +26,6 @@ background-size: 100% 100%">
 </div>
 </div>
 <?php
-$firstname=$_POST['firstname'];
-$email=$_POST['email'];
-$servername = "intelli.coghw13fheqo.us-east-2.rds.amazonaws.com";
-$username = "amdmin";
-$password = "intel123";
-$db = "intel";
-// Create connection
-$conn = new mysqli($servername, $username, $password, $db);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-if(isset($_POST['firstname']) && isset($_POST['email'])){
-$sql = "INSERT INTO data (firstname,email)
-VALUES ('".$firstname."', '".$email."')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
-}
-?>
-</body>
-</html>
 
 // Enable autoload via Composer
     require 'vendor/autoload.php';
